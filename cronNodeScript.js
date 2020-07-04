8216ef22a76600c49b40c02f5b92aec071ec1b85
Jawakar <jawakarsmith@gmail.com>
2020-07-04 18:51:56 +0530

    cron init

--- cronNodeScript.js
+++ cronNodeScript.js
@@ -1,16 +1,6 @@
-var cron = require('node-cron');
+const cron = require('node-cron');
 
 //execute every 1 min
 cron.schedule('*/1 * * * *', function(){
-    var shell = require('./child_helper');
-
-    var commandList = [
-        "node script1.js",
-        "npm run script -- PeterGood"
-    ]
-
-    shell.series(commandList , function(err){
-    //    console.log('execute many commands in a row'); 
         console.log('done')
-    });
 });
--- scriptParam.js
+++
@@ -1,6 +0,0 @@
-var param2 = process.argv[2] 
-var param3 = process.argv[3] 
-
-console.log('I am script with param : ')
-console.log('param2: ', param2)
-console.log('param3: ', param3)
